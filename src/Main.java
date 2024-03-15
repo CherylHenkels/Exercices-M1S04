306fb698a8173274d94516064adb2057e45757f2
CherylHenkels <cherylhenkelss@gmail.com>
2024-03-15 13:38:33 -0300

    M1S04: Exercicio4 - Lista de cursos e professores - Lista de cursos concluídos

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
         // ArrayList para armazenar lista de cursos e professores.
         List<String> cursosProfessores = new ArrayList<>();
 
+        // ArrayList para armazenar lista de cursos concluídos
+        List<String> cursosConcluidos = new ArrayList<>();
+
 
         System.out.println("*************");
         System.out.println("* BEM VINDO *");
@@ -17,13 +20,15 @@ public class Main {
 
 
         do {
-            System.out.println("|---------------|");
-            System.out.println("| 1- Listar     |");
-            System.out.println("| 2- Adicionar  |");
-            System.out.println("| 3- Remover    |");
-            System.out.println("|---------------|");
-            System.out.println("| 0- Sair       |");
-            System.out.println("|---------------|");
+            System.out.println("|--------------------------|");
+            System.out.println("| 1- Listar                |");
+            System.out.println("| 2- Adicionar             |");
+            System.out.println("| 3- Remover               |");
+            System.out.println("| 4- Marcar como concluído |");
+            System.out.println("| 5- Listar concluídos     |");
+            System.out.println("|--------------------------|");
+            System.out.println("| 0- Sair                  |");
+            System.out.println("|--------------------------|");
             System.out.println("Selecione uma opção:");
 
             int opcao = scanner.nextInt();
@@ -40,6 +45,12 @@ public class Main {
                 case 3: // remover
                     removerItemLista(scanner, cursosProfessores);
                     break;
+                case 4: // marcar como concluido
+                    marcarComoConcluido(scanner, cursosProfessores, cursosConcluidos);
+                    break;
+                case 5: // listar concluidos
+                    listarCursosProfessores(cursosConcluidos);
+                    break;
                 case 0: // sair
                     return;
                 default:
@@ -51,6 +62,17 @@ public class Main {
 
     }
 
+    private static void marcarComoConcluido(Scanner entrada, List<String> cursosProfessores, List<String> cursosConcluidos) {
+        int indice = listarPedirIndice(entrada, cursosProfessores);
+        for (int i = 0; i < cursosProfessores.size(); i++) {
+            if (i + 1 == indice) {
+                cursosConcluidos.add(cursosProfessores.get(i));
+                System.out.println("Curso " + cursosProfessores.get(i) + " foi concluído.");
+            }
+        }
+        removeritem(indice, cursosProfessores);
+    }
+
     private static void removerItemLista(Scanner entrada, List<String> cursosProfessores) {
         int indice = listarPedirIndice(entrada,cursosProfessores);
         removeritem(indice, cursosProfessores);
@@ -58,7 +80,7 @@ public class Main {
 
     private static void removeritem(int indice, List<String> cursosProfessores) {
         cursosProfessores.remove((indice - 1));
-        System.out.println("O curso/professor foi removido com sucesso!");
+        //System.out.println("O curso/professor foi removido com sucesso!");
     }
 
 
@@ -89,7 +111,8 @@ public class Main {
     private static void listarCursosProfessores(List<String> cursosProfessores) {
         System.out.println("\nSua lista de cursos/professores é:");
         for (int i = 0; i < cursosProfessores.size(); i++) {
-            System.out.println((i + 1) + " - " + cursosProfessores.get(i));
+            int indice = i + 1;
+            System.out.println(indice + " - " + cursosProfessores.get(i));
         }
     }
 
